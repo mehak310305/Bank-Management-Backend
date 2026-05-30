865cabc35a41d310f2bd3893359c4e0e09230fe0
mehak310305 <mehakb216@gmail.com>
2026-05-30 12:55:00 +0530

    Initial commit

---
+++ bankapp/src/main/java/com/bank/bankapp/controller/AuthController.java
@@ -0,0 +1,20 @@
+package com.bank.bankapp.controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.bank.bankapp.dto.LoginRequestDto;
+import com.bank.bankapp.dto.LoginResponseDto;
+import com.bank.bankapp.service.AuthService;
+import lombok.RequiredArgsConstructor;
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/auth")
+public class AuthController {
+  private final AuthService authService;
+  @PostMapping("/login")
+  public LoginResponseDto login(@RequestBody LoginRequestDto loginRequestDto)  {
+    return authService.login(loginRequestDto);
+  }
+}
+ 
